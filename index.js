36795cae61460220e3579cb829bfe565787bd033
Alice Li <aliceli083@gmail.com>
2023-05-25 12:22:47 -0700

    circle and square

---
+++ index.js
@@ -0,0 +1,56 @@
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+
+const questions = [{
+    type: 'list',
+    message: 'What shape should the logo be?',
+    name: 'shape',
+    choices:
+        [{
+            name: "Circle",
+            value: "circle",
+        },
+        {
+            name: "Square",
+            value: "rect",
+        },
+        {
+            name: "Triangle",
+            value: "polygon",
+
+        }]
+},
+{
+    type: 'input',
+    message: 'What color should the SVG be?',
+    name: 'shapeColor',
+},
+{
+    type: 'input',
+    message: 'What color should the SVG logo text be?',
+    name: 'textColor',
+},
+{
+    type: 'input',
+    message: 'What text should the SVG logo have?',
+    name: 'text',
+},
+]
+inquirer.prompt(questions).then(response => {
+    console.log(response)
+    if (response.shape === "circle") {
+        const circle = new Circle(response.shape, response.text, response.textColor, response.shapeColor)
+        fs.writeFileSync("logo.svg", circle.render())
+    }
+    if(response.shape ==="rect"){
+        const rect= new Square(response.shape, response.text, response.textColor, response.shapeColor)
+        fs.writeFileSync("logo.svg", rect.render())
+    }
+
+})
